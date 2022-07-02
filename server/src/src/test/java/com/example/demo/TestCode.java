9c11ba6df124e24f27b2542a4b146a28a83e2604
shinji-ono94 <1530sinnzi1003@gmail.com>
2022-07-02 17:17:27 +0900

    ERROR fixしない

--- server/src/src/test/java/com/example/demo/TestCode.java
+++ server/src/src/test/java/com/example/demo/TestCode.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TestCode {
 
 	@Test
 	void contextLoads() {
